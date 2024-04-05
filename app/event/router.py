94cfc33511666eb73f5bd4782ec8c7ea31e82609
0dmikrit <mai.grachev.dmitriy@gmail.com>
2024-04-05 03:41:45 +0300

    Незначительные фиксы системы

--- app/event/router.py
+++ app/event/router.py
@@ -1,5 +1,7 @@
 from app.event.service import EventsService
+from app.event.schemas import SEvents
 from fastapi import APIRouter
+from fastapi_cache.decorator import cache
 
 
 router = APIRouter(
@@ -9,6 +11,14 @@ router = APIRouter(
 
 
 @router.get("/")
-async def get_events():
+@cache(expire=60)
+async def get_events() -> list[SEvents]:
     result = await EventsService.get_all()
+    return result
+
+
+
+@router.get("/{id}")
+async def get_event(id: int) -> SEvents:
+    result = await EventsService.get_one_by_id(id)
     return result
