4e07e80c49c1a496396155d424c55a2350ee6310
0dmikrit <mai.grachev.dmitriy@gmail.com>
2024-04-09 23:01:28 +0300

    Исправление косяка с миграцией

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -15,7 +15,7 @@ sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 from app.config import DB_URL
 from app.database import Base
 from app.members.models import Members
-from app.event.models import Events
+from app.events.models import Events
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
